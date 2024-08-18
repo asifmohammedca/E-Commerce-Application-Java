b877d661fbed28f219a1aa10a492a793ecc04684
asifmohammedca <asifmohammedca96@gmail.com>
2024-08-18 11:02:35 +0200

    updated some properties

--- Services/Order/src/main/java/com/Ecommerce/Order/Model/Response/CustomerResponse.java
+++ Services/Order/src/main/java/com/Ecommerce/Order/Model/Response/CustomerResponse.java
@@ -2,8 +2,8 @@ package com.Ecommerce.Order.Model.Response;
 
 public record CustomerResponse(
         String id,
-        String firstname,
-        String lastname,
+        String firstName,
+        String lastName,
         String email
 ) {
 }
